184fb6494d9d52d523aad7ae05caaf388ac5d6f7
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-06-27 07:03:29 +0600

    Implement SignIn With Google

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -14,16 +14,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+
   user: {
     additionalFields: {
       role: {
-        defaultvalue: "member",
+        defaultValue: "member",
       },
       plan: {
-        defaultvalue: "free",
+        defaultValue: "free",
       },
       status: {
-        defaultvalue: "active",
+        defaultValue: "active",
       },
     },
   },
